c3866d515e3e134e5da25e289b1b46f80240b35a
Dellphix <bearabearybear@gmail.com>
2021-02-21 21:38:00 +1300

    Added Patchouli books mod dependancy

--- src/main/java/witchacademia/items/SpellBook.java
+++ src/main/java/witchacademia/items/SpellBook.java
@@ -1,11 +1,14 @@
 package witchacademia.items;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import vazkii.patchouli.api.PatchouliAPI;
 import witchacademia.WitchAcademia;
 
 public class SpellBook extends Item {
@@ -20,6 +23,10 @@ public class SpellBook extends Item {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
 
+        if (playerIn instanceof ServerPlayerEntity) {
+            System.out.println("Open book");
+            PatchouliAPI.instance.openBookGUI((ServerPlayerEntity) playerIn, Registry.ITEM.getKey(this));
+        }
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
